af94ff37ff27862cc43cdf620c866801b02c9f80
j_lyons2@u.pacific.edu <j_lyons2@u.pacific.edu>
2020-08-24 22:21:45 -0700

    Updates

---
+++ precache-manifest.196f5d5fb0f2f5d6d80fb042527544e2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cb4a96d3b18b710d9bf721bfa4b84dc5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d80ef9c930bd80707abc",
+    "url": "/static/css/main.f004bbcd.chunk.css"
+  },
+  {
+    "revision": "7d3466c67870e8e2a4ec",
+    "url": "/static/js/2.b6715e54.chunk.js"
+  },
+  {
+    "revision": "d80ef9c930bd80707abc",
+    "url": "/static/js/main.f43c0514.chunk.js"
+  },
+  {
+    "revision": "542e026f905a1820ba59",
+    "url": "/static/js/runtime-main.56d3e8a5.js"
+  }
+]);
